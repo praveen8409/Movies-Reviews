046208bd64390080d14fa367b8cbf27db1042b6a
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-09-21 21:28:57 +0530

    Database with Mongodb setup Done

---
+++ src/main/java/com/praveen/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
